069522ce04603b25bcb2295481670de9940d958f
lpestl <lpestlname@gmail.com>
2017-12-14 11:47:12 +0300

    Read array from input file

--- Task51_AliceBob/main.cpp
+++ Task51_AliceBob/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
 #include <Windows.h>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	ifstream input("input.txt");
+	ofstream output("output.txt", ios::out | ios::trunc);
+	
+	if (input.is_open()) {
+		vector<vector<int>> tArray;
+
+		std::string temp;
+
+		while (std::getline(input, temp)) {
+			std::istringstream buffer(temp);
+			std::vector<int> line { istream_iterator<int>(buffer),
+									   istream_iterator<int>() };
+
+			tArray.push_back(line);
+		}
+
+		for (auto& line : tArray)
+		{ 
+			for (auto& t : line) 
+			{
+				cout << t << " ";
+			}
+			cout << endl;
+		}
+	}
+	else {
+		output << "[ERROR] Input file was not found\n";
+	}
 
 	system("pause");
 	return 0;
