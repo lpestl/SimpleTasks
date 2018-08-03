8e2953de7011fb99387c069d144b944ca4a720fd
KataevMS <mikhail.kataev@rubius.com>
2018-08-03 11:51:06 +0300

    Added Fsharp and Csharp solution

--- Task114/Task114/Program.cs
+++ Task114/Task114/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task114
 {
@@ -10,6 +6,28 @@ namespace Task114
     {
         static void Main(string[] args)
         {
+            // 1, 5, 13, 29, 49, 81, 113, 149, 197, 253, 317 (последовательность A000328 в OEIS).
+            for (var i = 1; i <= 10; i++)
+                Console.WriteLine($"Radius = {i}; Answer = {NumberOfIntPoints(i)}");
+
+            Console.ReadKey();
+        }
+
+        // Количество целочисленных точек внутри круга
+        static int NumberOfIntPoints(int radius)
+        {
+            // счетчик
+            var res = 0;
+            // в цикле считаем количество целочисленных точек внутри одной четверти круга, (!!!) не лежащих на его осях (поэтому считаем не с нуля, а с единицы)
+            for (var x = 1; x <= radius; x++)
+                for (var y = 1; y <= radius; y++)
+                    if ((x * x + y * y) <= radius * radius)
+                        res++;
+                    else break;
+            // Результат умножаем на количество четвертей у круга, 
+            // добавляем количество точек лежащих на осях в четыре стороны от центра,
+            // и добавляем центр окружности (по условиям задачи он целочисленный)
+            return 1 + 4 * radius + 4 * res;
         }
     }
 }
---
+++ Task114/Task114FS/Program.fs
@@ -0,0 +1,23 @@
+﻿// Количество целочисленных точек внутри круга
+let rec NumberOfIntPoints radius =
+    // счетчик
+    let mutable res = 0
+    // в цикле считаем количество целочисленных точек внутри одной четверти круга, 
+    for x in 1..radius do
+        // (!!!) не лежащих на его осях (поэтому считаем не с нуля, а с единицы)
+        let mutable y = 1
+        while ((y <= radius) && ((x * x + y * y) <= radius * radius)) do
+            res <- res + 1
+            y <- y + 1
+    // Результат умножаем на количество четвертей у круга, 
+    // добавляем количество точек лежащих на осях в четыре стороны от центра,
+    // и добавляем центр окружности (по условиям задачи он целочисленный)
+    1 + 4 * radius + 4 * res
+
+[<EntryPoint>]
+let main argv = 
+    // 1, 5, 13, 29, 49, 81, 113, 149, 197, 253, 317 (последовательность A000328 в OEIS).
+    for i in 1..10 do
+        let answer = NumberOfIntPoints i
+        printfn "Radius = %A; Answer = %A" i answer
+    0 // return an integer exit code
