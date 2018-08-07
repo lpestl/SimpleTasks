5e73c388303df9e42e6353d14d65f29e82e6fe9a
Mikhail S. Kataev <lpestlname@gmail.com>
2018-08-07 22:01:18 +0300

    Added cshart solution

---
+++ Task115/Task115cs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task115cs
+{
+    class Program
+    {
+        // Test
+        static void Main(string[] args)
+        {
+            var k = 3; var fans = new[] {7, 12, 5};
+            var answer = ColculateHotelsRooms(k, fans);
+            Console.WriteLine($"{answer}");
+
+            k = 5; fans = new[] {12, 15, 21};
+            answer = ColculateHotelsRooms(k, fans);
+            Console.WriteLine($"{answer}");
+        }
+
+        private static int ColculateHotelsRooms(int k, int[] fans)
+        {
+            var res = 0;
+            foreach (var fan in fans)
+            {
+                // Разбиваем фанатов по командам
+                var fansWithoutHousing = fan;
+                // И заселяем их в комнаты до тех пор, пока все фанаты этой команды не будут заселены
+                while (fansWithoutHousing > 0)
+                {
+                    fansWithoutHousing -= k;
+                    // И считаем количество комнат
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
