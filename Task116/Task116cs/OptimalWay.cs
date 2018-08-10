bfd5ab388bc07305de3494072005fdc0dc832384
KataevMS <mikhail.kataev@rubius.com>
2018-08-10 11:16:01 +0300

    Added started project for task

---
+++ Task116/Task116cs/OptimalWay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task116cs
+{
+    public static class OptimalWay
+    {
+        public static List<ulong> GetOptimalWay(List<float> oilCosts, List<ulong[]> trainRoads)
+        {
+            var way = new List<ulong>();
+
+            return way;
+        }
+    }
+}
---
+++ Task116/UnitTests/BlackBoxTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using System.Text;
+using System.Threading.Tasks;
+using Task116cs;
+using Xunit;
+
+namespace UnitTests
+{
+    [Collection("Sequential")]
+    public class BlackBoxTests
+    {
+        [Fact]
+        public void Test00_FromTask()
+        {
+            var oilCoasts = new List<float>() { 5, 10, 1 };
+            var trainRoads = new List<ulong[]>() { new ulong[]{1, 3}, new ulong[] {1, 2}, new ulong[]{3, 2}};
+            var answer = OptimalWay.GetOptimalWay(oilCoasts, trainRoads);
+
+            answer.Count.Should().Be(2);
+            answer[0].Should().Be(1);
+            answer[1].Should().Be(3);
+        }
+    }
+}
