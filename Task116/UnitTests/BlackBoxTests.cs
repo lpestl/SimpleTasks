1a6761814c7263678b446d017942dee3bd7dbe6f
KataevMS <mikhail.kataev@rubius.com>
2018-08-10 15:58:25 +0300

    Dexter algorithm

--- Task116/UnitTests/BlackBoxTests.cs
+++ Task116/UnitTests/BlackBoxTests.cs
@@ -15,13 +15,42 @@ namespace UnitTests
         [Fact]
         public void Test00_FromTask()
         {
-            var oilCoasts = new List<float>() { 5, 10, 1 };
-            var trainRoads = new List<ulong[]>() { new ulong[]{1, 3}, new ulong[] {1, 2}, new ulong[]{3, 2}};
-            var answer = OptimalWay.GetOptimalWay(oilCoasts, trainRoads);
+            var oilCoasts = new int[] { 5, 10, 1 };
+            var trainRoads = new int[,] { {1, 3}, {1, 2}, {3, 2} };
+
+            var a = OptimalWay.GetOptimalWay(oilCoasts, trainRoads);
+            var answer = a.ToList();
 
             answer.Count.Should().Be(2);
             answer[0].Should().Be(1);
             answer[1].Should().Be(3);
+        }
+
+        [Fact]
+        public void Test01_NullTest()
+        {
+            var oilCoasts = new int[] { 5 };
+            var trainRoads = new int[,] { };
+
+            var a = OptimalWay.GetOptimalWay(oilCoasts, trainRoads);
+            var answer = a.ToList();
+
+            answer.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test02_FivePointGraph()
+        {
+            var oilCoasts = new int[] { 10, 20, 30, 40, 50 };
+            var trainRoads = new int[,] { {1,2}, {1,3}, {1,4}, {2,4}, {3, 5}, {3, 4}, {4, 5} };
+
+            var a = OptimalWay.GetOptimalWay(oilCoasts, trainRoads);
+            var answer = a.ToList();
+
+            answer.Count.Should().Be(3);
+            answer[0].Should().Be(1);
+            answer[1].Should().Be(3);
+            answer[2].Should().Be(5);
         }
     }
 }
