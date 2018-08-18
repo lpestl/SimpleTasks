93123af9558b711b79578bd673d6148df1ecbe5e
Mikhail S. Kataev <lpestlname@gmail.com>
2018-08-18 10:40:15 +0300

    Added empty project for task 118

---
+++ Task118/Task118Tests/SimpleTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Task118cs;
+using Xunit;
+
+namespace Task118Tests
+{
+    [Collection("Sequential")]
+    public class SimpleTests
+    {
+        [Fact]
+        public void Test00_TaskTest()
+        {
+            int[] values = { 1, 1, 0, 1 };
+            int[,] edges = { { 1, 2 }, { 1, 3 }, { 2, 4 } };
+            var answer = Tree.CalculateMaxSumUnboundVerhies(values, edges);
+            answer.Should().Be(2);
+        }
+    }
+}
---
+++ Task118/Task118cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task118cs
+{
+    public static class Tree
+    {
+        public static int CalculateMaxSumUnboundVerhies(int[] values, int[,] edges)
+        {
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = {1, 1, 0, 1};
+            int[,] edges = { { 1, 2}, { 1, 3}, { 2, 4} };
+            var answer = Tree.CalculateMaxSumUnboundVerhies(values, edges);
+            Console.WriteLine(answer);
+        }
+    }
+}
