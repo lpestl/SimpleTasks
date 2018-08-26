bab51c57f83dd972740b4e3efaea7108fee76c31
Mikhail S. Kataev <lpestlname@gmail.com>
2018-08-26 23:15:14 +0300

    First version algaritm

--- Task120/Task120fs/Program.fs
+++ Task120/Task120fs/Program.fs
@@ -1,4 +1,8 @@
-﻿type FrontLine = 
+﻿open System.Net.NetworkInformation
+
+open System.Collections.Generic
+
+type FrontLine = 
     { 
         FrontLine: uint32 
         PerimeterR: uint32 
@@ -11,25 +15,86 @@ type NeighborStatus =
     | ContactLine
     | Our
 
-type Direction = | N | NE | E | SE | S | SW | W | NW
+type Direction = | N | E | S | W
+
+type OperationStatus = | InProcess | MissionComplete
 
 type Position = 
     struct
-        val x: uint32
-        val y: uint32
+        val mutable x: uint32
+        val mutable y: uint32
         new (_x: uint32, _y: uint32) = { x = _x; y = _y }
     end
 
 type IntelligenceKey =
     struct
-        val position: Position
-        val direction: Direction
+        val mutable position: Position
+        val mutable direction: Direction
         new (pos: Position, dir: Direction) = { position = pos; direction = dir }
     end
 
-type MilitaryIntelligence (landingPoint: Position) =
-    let mutable currentPosition = landingPoint
-    
+type MilitaryIntelligence (landingPoint: Position,
+                           area: _[,]) =
+    let map = new Dictionary<IntelligenceKey, NeighborStatus>()
+    let mutable directionIntelligence = IntelligenceKey(landingPoint, Direction.N)
+    let warArea = area
+    let mutable status = OperationStatus.InProcess
+
+    member x.Position = directionIntelligence.position
+
+    member x.Direction = directionIntelligence.direction
+
+    member x.CheckNeighbor (dir: Direction) : NeighborStatus =
+        let mutable whoIsNeighbor = NeighborStatus.NotExplored
+        if ((dir = N && x.Position.y = 0u) || 
+            (dir = S && x.Position.y = uint32(warArea.GetLength(1)) - 1u) ||
+            (dir = E && x.Position.x = uint32(warArea.GetLength(0)) - 1u) ||
+            (dir = W && x.Position.x = 0u)) then
+            whoIsNeighbor <- NeighborStatus.Perimeter
+        else 
+            let mutable lookCell = x.Position
+            match dir with
+            | N -> lookCell.y <- lookCell.y - 1u
+            | S -> lookCell.y <- lookCell.y + 1u
+            | E -> lookCell.x <- lookCell.x + 1u
+            | W -> lookCell.x <- lookCell.x - 1u
+
+            if warArea.[int(lookCell.x), int(lookCell.y)] = warArea.[int(x.Position.x), int(x.Position.y)] then
+                whoIsNeighbor <- NeighborStatus.Our
+            else 
+                whoIsNeighbor <- NeighborStatus.ContactLine
+        map.Add(IntelligenceKey(x.Position, dir), whoIsNeighbor)
+        whoIsNeighbor   
+
+    member x.NextStep : OperationStatus = 
+        let mutable dir = directionIntelligence.direction
+        let mutable lastDir = directionIntelligence.direction
+        while not (x.CheckNeighbor(dir) = Our) && not (map.ContainsKey(IntelligenceKey(x.Position, dir))) do
+            lastDir <- dir
+            match dir with
+            | N -> dir <- E
+            | E -> dir <- S
+            | S -> dir <- W
+            | W -> dir <- N
+        
+        if map.ContainsKey(IntelligenceKey(x.Position, dir)) then
+            status <- OperationStatus.MissionComplete
+        else 
+            if lastDir = dir then
+                match dir with
+                | N -> lastDir <- W
+                | E -> lastDir <- N
+                | S -> lastDir <- E
+                | W -> lastDir <- S
+            directionIntelligence.direction <- lastDir
+            match dir with
+            | N -> directionIntelligence.position.y <- directionIntelligence.position.y - 1u
+            | E -> directionIntelligence.position.x <- directionIntelligence.position.x + 1u
+            | S -> directionIntelligence.position.y <- directionIntelligence.position.y + 1u
+            | W -> directionIntelligence.position.x <- directionIntelligence.position.x - 1u
+        status
+        
+
 let rec GetFrontLine (area : _[,]) : FrontLine =
     let result = { FrontLine = 0u; PerimeterR = 0u; PerimeterF = 0u; }
 
@@ -37,6 +102,11 @@ let rec GetFrontLine (area : _[,]) : FrontLine =
         if (area.GetLength(1) <> 0) then
             let a = 5
             printf "%A" a
+            //let map = new Dictionary<IntelligenceKey, NeighborStatus>()
+            //let mutable directionIntelligence = IntelligenceKey(currentPosition, Direction.N)
+            //while (not map.ContainsKey(directionIntelligence)) do
+            //    let a = 6
+            //    printf "%A" a
 
     result
 
@@ -46,6 +116,13 @@ let main argv =
     let key2 = IntelligenceKey(Position(1u, 2u), Direction.N)
 
     if (key1 = key2) then
+        printf "Victory!!!\n"
+    else 
+        printf "Fail ((\n"
+
+    let mutable key3 = IntelligenceKey(Position(0u, 2u), Direction.N)
+    key3.position.x <- 1u
+    if (key1 = key3) then
         printf "Victory!!!\n"
     else 
         printf "Fail ((\n"
