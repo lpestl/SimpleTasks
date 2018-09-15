0a354f716271ae5128ceedf57c5b02af30da9626
Mikhail S. Kataev <lpestlname@gmail.com>
2018-09-15 14:42:40 +0300

    Try O(1)

--- Task126/Task126cpp/main.cpp
+++ Task126/Task126cpp/main.cpp
@@ -33,10 +33,12 @@ double calculacte_p_Bernoulli(unsigned int n, unsigned int k, double probability
 unsigned int number_of_possible_options(std::vector<char> source_alphabet, unsigned int lenght_option, std::string except_substring) 
 {
 	auto all_option = static_cast<unsigned int>(pow(source_alphabet.size(), lenght_option));
-	auto p = calculacte_p_Bernoulli(lenght_option, except_substring.length(),
-		static_cast<double>(lenght_option - except_substring.length()) / source_alphabet.size()) * all_option;
+	auto p = static_cast<double>(1) / source_alphabet.size();
+	auto p_start_substring = 1 - calculacte_p_Bernoulli(lenght_option - (except_substring.size() - 1), 0, p);
+	auto p_other_char_substring = pow(p, except_substring.size() - 1);
+	auto p_common = p_start_substring * p_other_char_substring;
 
-	return all_option - p;
+	return all_option - p_common * all_option;
 }
 
 int main() 
