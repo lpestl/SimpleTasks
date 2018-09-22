8f5125ab62bfbdbf46cf1f0f3bbbfcac6e07b077
Mikhail S. Kataev <lpestlname@gmail.com>
2018-09-22 16:01:03 +0300

    Added empty cpp project

---
+++ Task128/Task128cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+unsigned long long split_summand_on_max_multipl(unsigned n)
+{
+	unsigned long long answer = 0;
+
+	return answer;
+}
+
+int main()
+{
+	// Test from task
+	unsigned n = 5;
+	std::cout << split_summand_on_max_multipl(n) << std::endl;
+
+	return 0;
+}
